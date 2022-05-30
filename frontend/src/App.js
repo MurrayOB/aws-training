9f8f021d4a135994ebabe89b9c3bbee5df1659dc
MurrayOB <murray.obrien@matogen.com>
2022-05-30 15:25:41 +0200

    next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Latest AWS.</p>
+        <p>AWS.</p>
       </header>
     </div>
   );
